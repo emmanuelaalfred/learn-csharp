3d8551a6b3bcb5f6a7c06de9c488bd7c34f2fe49
emmanuelaalfred <161465931+emmanuelaalfred@users.noreply.github.com>
2024-03-13 14:24:46 +0000

    Clean up and Refactoring

--- AtmSimulator/Program.cs
+++ AtmSimulator/Program.cs
@@ -5,39 +5,56 @@ static class Program
 {
     static void Main()
     {
-        Console.WriteLine("Please enter the amount you would like to withdraw: ");
-        string input = Console.ReadLine();
-
-        int amount = int.Parse(input);
+        int amount = GetUserAmount();
 
         if (amount >= 600)
         {
             Console.WriteLine("This ATM can only dispense amounts up to £600.");
-
+            return;
         }
-        else if (amount % 10 != 0)
+
+        if (IsAmountAMulitipleOf10(amount))
         {
             Console.WriteLine("This ATM can only dispense amounts in multiples of 10.");
-
-        }
-        else
-        {
-            Console.WriteLine($"Dispense ${amount}");
-
-            int numberOf50 = amount / 50;
-            amount = amount - 50 * numberOf50;
-
-            int numberOf20 = amount / 20;
-            amount = amount - 20 * numberOf20;
-
-            int numberOf10 = amount / 10;
-
-
-            Console.WriteLine($"Dispensing {numberOf50} x 50 notes");
-            Console.WriteLine($"Dispensing {numberOf20} x 20 note");
-            Console.WriteLine($"Dispensing {numberOf10} x 10 noteS");
+            return;
         }
 
+        Console.WriteLine($"Dispense ${amount}");
+        DispenseNotes(amount);
     }
 
+    private static int GetUserAmount()
+    {
+        Console.Write("Please enter the amount you would like to withdraw: ");
+        string input = Console.ReadLine();
+        Console.WriteLine();
+        int amount = int.Parse(input);
+        return amount;
+    }
+
+    private static void DispenseNotes(int amount)
+    {
+        int numberOf50 = amount / 50;
+        amount -= 50 * numberOf50;
+
+        int numberOf20 = amount / 20;
+        amount -= 20 * numberOf20;
+
+        int numberOf10 = amount / 10;
+
+
+        if (numberOf50 != 0)
+            Console.WriteLine($"Dispensing {numberOf50} x 50 notes");
+
+        if (numberOf20 != 0)
+            Console.WriteLine($"Dispensing {numberOf20} x 20 notes");
+
+        if (numberOf10 != 0)
+            Console.WriteLine($"Dispensing {numberOf10} x 10 notes");
+    }
+
+    private static bool IsAmountAMulitipleOf10(int amount)
+    {
+        return amount % 10 != 0;
+    }
 }
